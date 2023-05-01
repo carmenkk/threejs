5ba45b448a95bb0aa7a89c9215f56ae4414e3743
carmenkuang87@gmail.com <carmenkuang87@gmail.com>
2023-05-01 16:54:22 -0400

    backdrop

--- client/src/canvas/Backdrop.jsx
+++ client/src/canvas/Backdrop.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+import { AccumulativeShadows, RandomizedLight } from '@react-three/drei';
 
 const Backdrop = () => {
   return (
-    <div>Backdrop</div>
+    <AccumulativeShadows>
+      <RandomizedLight
+      amount = {4}/>
+
+      
+    </AccumulativeShadows>
   )
 }
 
