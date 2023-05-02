e8bc0480d414e0ffd858f1910ef66df38982f754
carmenkuang87@gmail.com <carmenkuang87@gmail.com>
2023-05-01 23:45:04 -0400

    openai

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,11 +7,36 @@ dotenv.config();
 const router = express.Router();
 
 const config = new Configuration({
-  apiKey:
-})
+  apiKey:process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({message: "Hello from 2.0"})
+})
+
+router.route('/').post(async (req, res) => {
+  try {
+    const {prompt} = req.body;
+
+    const response = await openai.createImage({
+      prompt,
+      n: 1,
+      size: '1024x1024',
+      response_format: 'b24_json' 
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    //pass it to frontend
+
+    res.status(200).json({ photo: image});
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong"})
+  }
 })
 
 export default router;
